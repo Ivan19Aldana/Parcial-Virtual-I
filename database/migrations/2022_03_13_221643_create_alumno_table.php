30ccb193ce1848f5dbc25d2d14b651721944ce80
Ivan19Aldana <ivanuchi189@gmail.com>
2022-03-13 17:04:22 -0600

    se crearon las migraciones

---
+++ database/migrations/2022_03_13_221643_create_alumno_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAlumnoTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('alumno', function (Blueprint $table) {
+            $table->string('carné');
+            $table->string('nombre');
+            $table->string('alias');
+            $table->string('foto');
+            $table->string('correo');
+            $table->string('fecha_nacimiento');
+            $table->string('telefono');
+            $table->integer('id_categoria');
+
+
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('alumno');
+    }
+}
