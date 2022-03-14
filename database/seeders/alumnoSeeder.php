d123bd66d143aca719f5a4f458d0205c63297599
Ivan19Aldana <ivanuchi189@gmail.com>
2022-03-14 00:45:02 -0600

    Se crearon los seed para hacer la tabla con 2 registros

---
+++ database/seeders/alumnoSeeder.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\alumno;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class alumnoSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $foto = ('OPRdCCGb6A9nMhoWwQVpy221wDyydzaIsPcSveOx.jpg');
+
+        DB::table('alumno')->insert([
+            'carne' => '700',
+            'nombre' => 'IVAN ALDANA ',
+            'alias' => 'ALDANA',
+            'foto' =>$foto,
+            'correo' => 'aldana@gmail.com',
+            'fecha_nacimiento' => '2001-09-12',
+            'telefono' => '00004545',
+            'id_categoria' => '2',
+        ]);
+
+        DB::table('alumno')->insert([
+            'carne' => '701',
+            'nombre' => 'FABIO MORALES',
+            'alias' => 'FAB',
+            'foto' =>$foto,
+            'correo' => 'faby@gmail.com',
+            'fecha_nacimiento' => '2002-11-31',
+            'telefono' => '78963120',
+            'id_categoria' => '2',
+        ]);
+
+
+    }
+}
---
+++ database/seeders/categoriaSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+use App\Models\categoria;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class categoriaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('categoria')->insert([
+            'id_categoria' => '1',
+            'descripcion' => 'BUENO'
+        ]);
+
+        DB::table('categoria')->insert([
+            'id_categoria' => '2',
+            'descripcion' => 'REGULAR'
+        ]);
+
+        DB::table('categoria')->insert([
+            'id_categoria' => '3',
+            'descripcion' => 'MALO'
+        ]);
+
+    }
+}
